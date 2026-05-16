62a1fe5fd80951a67bcbabe64c37d9ab81005061
murilo-a-souza <sir.murilos@gmail.com>
2026-05-16 11:37:40 -0300

    feat : Classe usuário finalizada, com ajustes no diagrama.

--- java/projeto-java/src/br/com/fiap/bean/Usuario.java
+++ java/projeto-java/src/br/com/fiap/bean/Usuario.java
@@ -1,21 +1,23 @@
 package br.com.fiap.bean;
 
-import java.util.ArrayList;
+import javax.swing.*;
 
 public class Usuario {
     private String nome;
     private String id;
     private float confiabilidade;
     private int soulCoins;
-    //Única coisa não aprendida, pesquisei para se encaixar na ideia.
-    private ArrayList<String> atividadeRecente;
+    //Um valor "vazio" é definido para não dar erro na concatenação de strings.
+    private String atividadeRecente = "";
+
     public Usuario (){}
     //Construtor para registrar usuário (nome, id e confiabilidade).
-    public Usuario(float confiabilidade, String id, String nome) {
-        this.confiabilidade = confiabilidade;
-        this.id = id;
+    public Usuario(String nome, String id, float confiabilidade) {
         this.nome = nome;
+        setId(id);
+        setConfiabilidade(confiabilidade);
     }
+
     public String getNome() {
         return nome;
     }
@@ -25,14 +27,24 @@ public class Usuario {
     public String getId() {
         return id;
     }
+    // A id é sempre minúscula.
     public void setId(String id) {
-        this.id = id;
+        this.id = id.toLowerCase();
     }
     public float getConfiabilidade() {
         return confiabilidade;
     }
+    // Confiabilidade varia entre 0 e 100%, e é mostrada como porcentagem
     public void setConfiabilidade(float confiabilidade) {
-        this.confiabilidade = confiabilidade;
+        try {
+            if (confiabilidade >= 0 && confiabilidade <= 100) {
+                this.confiabilidade = confiabilidade;
+            } else {
+                throw new Exception("Valor inválido, digite um número de 1 a 100 para confiabilidade");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        }
     }
     public int getSoulCoins() {
         return soulCoins;
@@ -40,19 +52,35 @@ public class Usuario {
     public void setSoulCoins(int soulCoins) {
         this.soulCoins = soulCoins;
     }
-    public ArrayList<String> getAtividadeRecente() {
+    public String getAtividadeRecente() {
         return atividadeRecente;
     }
     public void setAtividadeRecente(String atividadeRecente) {
-        this.atividadeRecente.addFirst(atividadeRecente);
+        this.atividadeRecente = atividadeRecente;
     }
+
     public void registrarPontos(int soulCoins, String descAcao){
-        this.soulCoins = soulCoins;
-        setAtividadeRecente(String.format("%s\n",
-                descAcao));
+        this.soulCoins += soulCoins;
+        this.atividadeRecente = this.atividadeRecente.concat(descAcao+"\n");
     }
     public String detalhesPerfil(){
-        return String.format("Nome: %s\nId: %s\nConfiabilidade: %.3f\nSoul Coins: %d\nAtividade recente:\n%s",
+        return String.format("Nome: %s\nId: %s\nConfiabilidade: %.1f\nSoul Coins: %d\nAtividade recente:\n%s",
                 nome, id, confiabilidade, soulCoins, atividadeRecente);
+    }
+    //Retgistra uma penalidade d confiabilidade para o usuário, e retorna uma string indicando o registro.
+    public String registrarPenalidade(float valorPenalidade, String descOcorrido){
+        //Recebe valores positivos e não pode receber negativs, por isso o try
+        try {
+            // 0 vai permitir que seja "cancelado"
+            if (valorPenalidade >= 0) {
+                /*Se o valor da penalidade for maio que o valor atual então ele vai ser colocado como zero
+                Isso impede que a confiablidade fique negativa, basicamente o mathmax vai pegar o maior valor, se o resultado for negativo será o 0*/
+                this.confiabilidade = Math.max(0, this.confiabilidade -= valorPenalidade);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", JOptionPane.ERROR_MESSAGE);
+        }
+        return String.format("Penalidade registrada, %s perdeu %.1f%% de confiabilidade, ficando com %.1f%%. Desc: %s",
+                nome, valorPenalidade, confiabilidade, descOcorrido);
     }
 }
--- java/projeto-java/src/br/com/fiap/main/Main.java
+++ java/projeto-java/src/br/com/fiap/main/Main.java
@@ -1,4 +1,9 @@
 package br.com.fiap.main;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
